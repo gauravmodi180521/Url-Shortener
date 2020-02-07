5a62fc35e27c3ef882653dac387f26141f364213
Gaurav Modi <gauravmodi18052000@gmail.com>
2020-02-07 10:03:01 +0530

    initial commit

---
+++ urlshortener/src/main/java/com/gaurav/urlshortener/models/Url.java
@@ -0,0 +1,40 @@
+package com.gaurav.urlshortener.models;
+
+public class Url {
+    public String originalUrl;
+    public String shortenedUrl;
+    public String creationDate;
+    public String userId;
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortenedUrl() {
+        return shortenedUrl;
+    }
+
+    public void setShortenedUrl(String shortenedUrl) {
+        this.shortenedUrl = shortenedUrl;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
---
+++ urlshortener/src/main/java/com/gaurav/urlshortener/models/User.java
@@ -0,0 +1,31 @@
+package com.gaurav.urlshortener.models;
+
+public class User {
+    public String Id;
+    public String name;
+    public String email;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
